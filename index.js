c3c0d010f6fe3d8b780b4096c5b2d91ed7afd4c0
shahinexy <shahinexy@gmail.com>
2024-05-08 09:26:18 +0600

    change uri to connect to my database

--- index.js
+++ index.js
@@ -8,11 +8,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
+// EnaJohn
+// vmxxoNh2EwCrJJPf
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.76h69in.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+console.log(process.env.DB_USER , process.env.DB_PASS);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
